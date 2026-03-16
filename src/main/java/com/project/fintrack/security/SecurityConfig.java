b5c686d14fe9da8d3a6501c6bf190b838f5debac
muskan shukla <muskanshukla707@gmail.com>
2026-03-16 15:27:18 +0530

    cors issue fixed

--- src/main/java/com/project/fintrack/security/SecurityConfig.java
+++ src/main/java/com/project/fintrack/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     	http
-            .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/userRegistration"))
             .cors(cors -> {})
             .authenticationProvider(authenticationProvider())
             .authorizeHttpRequests(auth -> auth
